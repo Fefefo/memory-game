a2c7325577de7cdc609b568ce07949589d5782f1
Fefefo <tardiolifederico@gmail.com>
2025-04-19 04:16:05 +0200

    Restyling

--- src/routes/[game]/+page.svelte
+++ src/routes/[game]/+page.svelte
@@ -30,7 +30,7 @@
     currEls = [];
     completedEls = [];
     startTime = undefined;
-    dialogOpen = true;
+    dialogOpen = false;
 
     boardKey += 1;
     ready = true;
@@ -83,21 +83,24 @@
   <AlertDialog.Portal>
     <AlertDialog.Overlay class="fixed inset-0 z-50 bg-black/80" />
     <AlertDialog.Content
-      class="fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-md border-2 border-[#235] bg-[#457] p-7 outline-hidden sm:max-w-lg md:w-full"
+      class="bg-primary border-border fixed top-[50%] left-[50%] z-50 flex w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] flex-col items-center gap-4 rounded-md border-2 p-7 text-white outline-hidden sm:max-w-lg md:w-full"
     >
-      <AlertDialog.Title>Well Done!</AlertDialog.Title>
-      <AlertDialog.Description
-        >You win in {minElapsed > 0
+      <AlertDialog.Title class="text-2xl">Congratulations</AlertDialog.Title>
+      <AlertDialog.Description class="text-offwhite"
+        >You won in {minElapsed > 0
           ? `${minElapsed}m ${secElapsed}s`
           : `${secElapsed} seconds`}!</AlertDialog.Description
       >
-      <AlertDialog.Action onclick={initBoard}>YAAAY</AlertDialog.Action>
+      <AlertDialog.Action
+        class="hover:motion-preset-shake w-1/2 rounded bg-blue-600/35 px-4 py-3 font-bold shadow-md"
+        onclick={initBoard}>Play again!</AlertDialog.Action
+      >
     </AlertDialog.Content>
   </AlertDialog.Portal>
 </AlertDialog.Root>
 
-<div class="min-h-screen overflow-hidden bg-[#404258] select-none">
-  <main class="mx-auto mt-5 flex max-w-5xl flex-col rounded-lg bg-[#474e68] p-8 text-center align-middle text-white">
+<div class="bg-board sm:bg-surface min-h-screen overflow-hidden select-none">
+  <main class="bg-board mx-auto my-5 flex max-w-5xl flex-col rounded-lg p-8 text-center align-middle text-white">
     <section class="flex flex-col items-center">
       <h3 class="text-center text-2xl font-bold">
         {data.title}
@@ -126,23 +129,17 @@
         >
           {#key boardKey}
             {#each characters as character}
-              <div
-                class="card-container relative {width == 8
-                  ? 'h-10 w-10'
-                  : width == 6
-                    ? 'h-12 w-12'
-                    : 'h-[60px] w-[60px]'} sm:h-[60px] sm:w-[60px]"
-              >
+              <div class="relative {width == 8 ? 'h-10 w-10' : width == 6 ? 'h-12 w-12' : 'h-16 w-16'} sm:h-16 sm:w-16">
                 <button
                   class="flex h-full w-full rounded duration-500 transform-3d"
                   onmouseup={flipCard}
                   data-name={character}
                 >
                   <div
-                    class="align-items absolute box-border flex h-full w-full justify-center rounded-xl border-2 border-cyan-700 bg-[#457] shadow-sm backface-hidden"
+                    class="align-items border-border bg-tertiary absolute box-border flex h-full w-full justify-center rounded-xl border-2 shadow-sm backface-hidden"
                   ></div>
                   <div
-                    class="card-bg align-items absolute flex h-full w-full rotate-y-180 justify-center rounded-xl backface-hidden"
+                    class="border-card-border absolute box-border flex h-full w-full rotate-y-180 justify-center rounded-xl border-2 backface-hidden"
                   >
                     <img class="rounded-xl" src={ReplacePlaceholders(data.picsURI, { character })} alt="" />
                   </div>
@@ -154,16 +151,9 @@
       {/if}
     </section>
     <section>
-      <button
-        class="hover:motion-preset-shake mt-5 rounded bg-[#50577a] px-4 py-3 font-bold shadow-md"
-        onclick={swapGame}>SWAP GAME</button
+      <button class="hover:motion-preset-shake bg-primary mt-5 rounded px-4 py-3 font-bold shadow-md" onclick={swapGame}
+        >SWAP GAME</button
       >
     </section>
   </main>
 </div>
-
-<!-- <style lang="postcss">
-  .card-size {
-    @apply h-12 w-12 sm:h-[60px] sm:w-[60px];
-  }
-</style> -->
